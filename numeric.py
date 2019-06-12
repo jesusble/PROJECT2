d39d862ffcbdfccf14810050af7e52e2dfc2de34
jesusble <keziahblessy33@gmail.com>
2019-06-12 14:35:50 +0530

    Create numeric.py

---
+++ numeric.py
@@ -0,0 +1,34 @@
+
+S=int(input())
+for i in range(0,10):
+	if(i==1):
+		print("one")
+		break
+	if(i==2):
+		print("two")
+		break
+	if(i==3):
+		print("three")
+		break
+	if(i==4):
+		print("four")
+		break
+	if(i==5):
+		print("five")
+		break
+	if(i==6):
+		print("six")
+		break
+	if(i==7):
+		print("seven")
+		break
+	if(i==8):
+		print("eight")
+		break
+	if(i==9):
+		print("nine")
+		break
+	if(i==10):
+		print("ten")
+		break
+
